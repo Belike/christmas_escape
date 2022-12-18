14274e5ffb7a4da9509f10f116ae9ad50100a6b1
Norman Lüring <norman.luering@tu-dortmund.de>
2022-12-18 01:32:39 +0100

    Deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/christmas_escape/" : "/",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/christmas_escape/'
+    : '/',
 
   pluginOptions: {
     vuetify: {
